0f8ac43275c6edc3e48d681d4fd086c14e430e1e
ASIF <aa2312000@gmail.com>
2022-06-05 10:13:36 +0530

    url dispatcher completed

---
+++ django/first/home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<b>This is HomePage<b>")
+
+def about(request):
+    return HttpResponse("yhis is home page")
+    
+def services(request):
+    return HttpResponse("service page")
+
+def contact(request):
+    return HttpResponse("contact page")

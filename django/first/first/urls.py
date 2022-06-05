8756a99db70cd2eef642cabcda0883d9f1f0be78
ASIF <aa2312000@gmail.com>
2022-06-05 10:57:00 +0530

    admin portal changes in django

--- django/first/first/urls.py
+++ django/first/first/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_header = "FIrst Django Application "
+admin.site.site_title = "First App Admin Portal"
+admin.site.index_title = "Welcome to first app"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),

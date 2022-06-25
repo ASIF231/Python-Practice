b0127a2c13dd22b29d781c4f6ce38be22ed96756
MAHAMADASIF ANSARI <maan@odoo.com>
2022-06-25 09:42:05 +0530

    imp

--- advance/decorator.py
+++ advance/decorator.py
@@ -1,9 +1,15 @@
-def add(x, y):  
-    return x+y  
-def sub(x ,y):  
-    return x-y  
-def operator(func, x, y):  
-    temp = func(x, y)  
-    return temp  
-print(operator(sub,10, 5))  
-print(operator(add,20, 5)) 
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def operator(func, x, y):
+    temp = func(x, y)
+    return temp
+
+
+print(operator(sub, 10, 5))
+print(operator(add, 20, 5))
---
+++ advance/variables.py
@@ -0,0 +1,27 @@
+var1 = "Hello world! "
+var4 = "ASIF"
+
+var2 = 4
+var3 = 35.5
+
+print(var1 + var4)
+print(type(var1))
+
+var1 = "30"
+var2 = "30"
+print(int(var1) + int(var2))
+print(100 * str(int(var1) + int(var2)))
+
+print(10 * "hello world\n")
+
+print('Enter any Number : ')
+inp = input()
+
+print("you Enetered plus 10 = ", int(inp)+10)
+
+print("Enter Number 1 :")
+n1 = input()
+print("Enter Number 2 :")
+n2 = input()
+
+print("sum = ", int(n1) + int(n2))
